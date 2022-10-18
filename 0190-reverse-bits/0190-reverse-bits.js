2dfd20fa59ce386addae812743573b55a9c16a2a
jo-ui <57154082+jo-ui@users.noreply.github.com>
2022-10-18 22:28:29 +0300

    Time: 84 ms (81.61%), Space: 44.2 MB (41.23%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function(n) {
+ 
+    let reversedArray = n.toString(2).split("").reverse()
+    while(reversedArray.length <32){ reversedArray.push('0')}
+    return  parseInt(reversedArray.join(""),2) 
+};
