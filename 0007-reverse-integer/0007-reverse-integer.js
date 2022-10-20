f3860c965ca8989f165f09038cd01792fcb66fff
jo-ui <57154082+jo-ui@users.noreply.github.com>
2022-10-20 22:25:59 +0300

    Time: 145 ms (13.55%), Space: 44.6 MB (9.04%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+   let max = (2 ** 31) - 1
+  let min = - (2 ** 31)
+  
+  const isMinus = x < 0
+  const str = String(Math.abs(x)).split('').reverse().join('')
+  const result =  isMinus ? Number(`-${str}`) : Number(str)
+  if (result > max || result < min) {
+    return 0
+  }
+  return result
+};
