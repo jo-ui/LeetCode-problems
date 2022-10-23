ddb15a64764242370dea4ce32a096a72124a3ec4
jo-ui <57154082+jo-ui@users.noreply.github.com>
2022-10-23 11:24:59 +0300

    Time: 102 ms (41.95%), Space: 42.7 MB (11.75%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const isSubsequence = (s = '', t = '') => {
+  let index = 0
+  for (let i = 0; i < t.length; i++) {
+    if (t[i] === s[index]) {
+      index += 1
+    }
+  }
+  return index >= s.length
+}
