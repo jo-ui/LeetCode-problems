45123e559a4784587cf1ef73b2bbbe62604b9507
jo-ui <57154082+jo-ui@users.noreply.github.com>
2022-11-03 23:39:48 +0300

    Time: 115 ms (64.74%), Space: 44.2 MB (13.97%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+  const memo = {}
+  const aux = (index) => {
+    if (memo[nums[index]] !== undefined) {
+      return [memo[nums[index]], index]
+    }
+    memo[target - nums[index]] = index
+    return aux(index + 1)
+  }
+  return aux(0, nums.length - 1)
+}
