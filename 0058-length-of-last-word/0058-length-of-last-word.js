722d94167ed01982c310ab4858dadad8015eaaee
jo-ui <57154082+jo-ui@users.noreply.github.com>
2022-12-09 23:50:21 +0300

    Commit

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -2,7 +2,7 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function (ss) {
-  const words = ss.trim().split(" ");
+var lengthOfLastWord = function (str) {
+  const words = str.trim().split(" ");
   return words.length > 0 ? words[words.length - 1].length : 0;
 };
